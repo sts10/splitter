9da7bde61bed86035317ee7a3be35ad497d8cbf4
Sergey Bugaev <bugaevc@gmail.com>
2021-04-13 11:11:39 +0300

    Improve the benchmark
    Use Criterion's benchmarking with inputs functionality to benchmark
    how the functions perform across a range of input sizes.
    
    https://bheisler.github.io/criterion.rs/book/user_guide/benchmarking_with_inputs.html
    https://bheisler.github.io/criterion.rs/book/user_guide/comparing_functions.html

--- benches/splitters.rs
+++ benches/splitters.rs
@@ -1,10 +1,28 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 
 use splitter::*;
 
+fn make_text(len: usize) -> String {
+    use std::iter::repeat;
+
+    let mut s = String::with_capacity(len);
+    // Start with some 'a' characters.
+    s.extend(repeat('a').take(len / 2));
+    // Then add a single ' '.
+    s.push(' ');
+    let remaining = len - 1 - (len / 2);
+    // Then fill the remaining space with more 'a's.
+    s.extend(repeat('a').take(remaining));
+
+    debug_assert_eq!(s.len(), s.capacity());
+    debug_assert_eq!(s.len(), len);
+    s
+}
+
 pub fn criterion_benchmark(c: &mut Criterion) {
+    const KB: usize = 1024;
+
     let mut group = c.benchmark_group("Splitters");
-    let sample_text = "word1 word2";
 
     let functions = [
         // ("original", remove_through_first_char),
@@ -14,13 +32,21 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         ("v4", remove_through_first_char_variant_4),
     ];
 
-    group.bench_function("original", |b| {
-        b.iter(|| remove_through_first_char(black_box(sample_text), ' '))
-    });
-    for &(name, function) in &functions {
-        group.bench_function(name, |b| {
-            b.iter(|| function(black_box(sample_text), ' '))
-        });
+    for &len in &[4, 8, 16, 256, KB, 4 * KB, 16 * KB, 32 * KB] {
+        let text = make_text(len);
+        group.throughput(Throughput::Bytes(len as u64));
+        // For moderate sizes, benchmark the slow function.
+        if len <= 16 * KB {
+            group.bench_with_input(BenchmarkId::new("original", len), &len, |b, _len| {
+                b.iter(|| remove_through_first_char(black_box(&text), ' '))
+            });
+        }
+        // Benchmark the fast functions.
+        for &(name, function) in &functions {
+            group.bench_with_input(BenchmarkId::new(name, len), &len, |b, _len| {
+                b.iter(|| function(black_box(&text), ' '))
+            });
+        }
     }
 }
 
