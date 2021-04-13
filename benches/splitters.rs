49b752129b043544a3d62e9364fc59d264eb388f
Sergey Bugaev <bugaevc@gmail.com>
2021-04-13 11:58:16 +0300

    Loop over functions for benchmarking

--- benches/splitters.rs
+++ benches/splitters.rs
@@ -1,5 +1,3 @@
-// lot of copy and pasting from:
-// https://bheisler.github.io/criterion.rs/book/getting_started.html
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use splitter::*;
@@ -8,21 +6,22 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("Splitters");
     let sample_text = "word1 word2";
 
-    group.bench_function("Basic", |b| {
+    let functions = [
+        // ("original", remove_through_first_char),
+        ("v1", remove_through_first_char_variant_1 as fn(&str, char) -> &str),
+        ("v2", remove_through_first_char_variant_2),
+        ("v3", remove_through_first_char_variant_3),
+        ("v4", remove_through_first_char_variant_4),
+    ];
+
+    group.bench_function("original", |b| {
         b.iter(|| remove_through_first_char(black_box(sample_text), ' '))
     });
-    group.bench_function("V1", |b| {
-        b.iter(|| remove_through_first_char_variant_1(black_box(sample_text), ' '))
-    });
-    group.bench_function("V2", |b| {
-        b.iter(|| remove_through_first_char_variant_2(black_box(sample_text), ' '))
-    });
-    group.bench_function("V3", |b| {
-        b.iter(|| remove_through_first_char_variant_3(black_box(sample_text), ' '))
-    });
-    group.bench_function("V4", |b| {
-        b.iter(|| remove_through_first_char_variant_4(black_box(sample_text), ' '))
-    });
+    for &(name, function) in &functions {
+        group.bench_function(name, |b| {
+            b.iter(|| function(black_box(sample_text), ' '))
+        });
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
